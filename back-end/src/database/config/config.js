487327495d0b48036591db2218718e456f645aaf
Juka <julianoprogrammierer@gmail.com>
2022-11-15 16:14:22 -0300

    config initial docker compose.

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -11,12 +11,12 @@ const suffix = {
 };
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
+  host: process.env.DB_HOST || 'localhost',
+  port: process.env.DB_PORT || '3306',
   database: 
     `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+  username: process.env.DB_USER || 'root',
+  password: process.env.DB_PASS || 'password',
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
