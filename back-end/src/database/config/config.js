3e7230facdc47b50bb7f2a1cc33b69aa1eb67d42
Juka <julianoprogrammierer@gmail.com>
2022-11-18 13:20:40 -0300

    config deploy

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,37 +1,16 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-// sufix
-const suffix = {
-  prod: "",
-  production: "",
-  dev: "-dev",
-  development: "-dev",
-  test: "-test",
-};
-
-const options = {
-  host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
-  port: process.env.MYSQL_PORT || '3306',
-  database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQL_USER || 'root',
-  password: process.env.MYSQL_PASSWORD || 'password',
+const config = {
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+  host: process.env.MYSQLHOST,
+  port: process.env.MYSQLPORT,
   dialect: 'mysql',
-  dialectOptions: {
-    timezone: 'Z',
-  },
-  logging: false,
 };
 
 module.exports = {
-  development: {
-    ...options,
-  },
-  test: {
-    ...options,
-  },
-  production: {
-    ...options,
-  },
+  development: config,
+  test: config,
+  production: config,
 };
