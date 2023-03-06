6d801bebde5a4525eef6263ed276cf2bb0053e91
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-03-06 16:37:11 -0300

    add more products for testing

--- back-end/src/database/seeders/1-products.js
+++ back-end/src/database/seeders/1-products.js
@@ -6,54 +6,54 @@ module.exports = {
       url_image: 'http://localhost:3001/images/skol_lata_350ml.jpg',
     },
     {
-      name: 'Heineken 600ml',
-      price: 7.50,
-      url_image: 'http://localhost:3001/images/heineken_600ml.jpg',
-    },
-    {
-      name: 'Antarctica Pilsen 300ml',
-      price: 2.49,
-      url_image: 'http://localhost:3001/images/antarctica_pilsen_300ml.jpg',
-    },
-    {
-      name: 'Brahma 600ml',
-      price: 7.50,
-      url_image: 'http://localhost:3001/images/brahma_600ml.jpg',
-    },
-    {
-      name: 'Skol 269ml',
-      price: 2.19,
-      url_image: 'http://localhost:3001/images/skol_269ml.jpg',
-    },
-    {
-      name: 'Skol Beats Senses 313ml',
-      price: 4.49,
-      url_image: 'http://localhost:3001/images/skol_beats_senses_313ml.jpg',
-    },
-    {
-      name: 'Becks 330ml',
+      name: 'Heineken Lata 350ml',
       price: 4.99,
-      url_image: 'http://localhost:3001/images/becks_330ml.jpg',
+      url_image: 'http://localhost:3001/images/heineken_lata_350ml.jpg',
     },
     {
-      name: 'Brahma Duplo Malte 350ml',
-      price:2.79,
-      url_image: 'http://localhost:3001/images/brahma_duplo_malte_350ml.jpg',
+      name: 'Antarctica Pilsen 600ml',
+      price: 5.49,
+      url_image: 'http://localhost:3001/images/antarctica_pilsen_600ml.jpg',
     },
     {
-      name: 'Becks 600ml',
-      price: 8.89,
-      url_image: 'http://localhost:3001/images/becks_600ml.jpg',
+      name: 'Brahma Lata 350ml',
+      price: 4.99,
+      url_image: 'http://localhost:3001/images/brahma_lata_350ml.jpg',
     },
     {
-      name: 'Skol Beats Senses 269ml',
-      price: 3.57,
-      url_image: 'http://localhost:3001/images/skol_beats_senses_269ml.jpg'
+      name: 'Skol Beats Senses 600ml',
+      price: 7.99,
+      url_image: 'http://localhost:3001/images/skol_beats_senses_600ml.jpg',
     },
     {
-      name: 'Stella Artois 275ml',
+      name: 'Becks Lata 350ml',
+      price: 3.99,
+      url_image: 'http://localhost:3001/images/becks_lata_350ml.jpg',
+    },
+    {
+      name: 'Brahma Duplo Malte 600ml',
+      price: 5.49,
+      url_image: 'http://localhost:3001/images/brahma_duplo_malte_600ml.jpg',
+    },
+    {
+      name: 'Heineken Lata 250ml',
       price: 3.49,
-      url_image: 'http://localhost:3001/images/stella_artois_275ml.jpg'
+      url_image: 'http://localhost:3001/images/heineken_lata_250ml.jpg',
+    },
+    {
+      name: 'Antarctica Pilsen Lata 350ml',
+      price: 3.99,
+      url_image: 'http://localhost:3001/images/antarctica_pilsen_lata_350ml.jpg',
+    },
+    {
+      name: 'Skol Beats Lata 350ml',
+      price: 4.79,
+      url_image: 'http://localhost:3001/images/skol_beats_senses_lata_350ml.jpg',
+    },
+    {
+      name: 'Stella Artois Lata 330ml',
+      price: 4.49,
+      url_image: 'http://localhost:3001/images/stella_artois_lata_330ml.jpg',
     },
     {
       name: 'Heineken 600ml',
