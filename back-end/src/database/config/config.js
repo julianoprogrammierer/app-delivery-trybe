56b193cc6f71bc96ea2c7d3024a417f7e463ce48
Juliano Roberto Machado <julianoprogrammierer@gmail.com>
2023-03-06 16:36:26 -0300

    change configs conection

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,7 +1,5 @@
 require('dotenv').config();
 
-const environment = process.env.NODE_ENV || "test";
-// sufix
 const suffix = {
   prod: "",
   production: "",
@@ -14,7 +12,7 @@ const options = {
   host: process.env.HOSTNAME || process.env.MYSQL_HOST || 'localhost',
   port: process.env.MYSQL_PORT || '3306',
   database: 
-    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+    `${process.env.MYSQL_DB_NAME || 'delivery-app'}${suffix[process.env.NODE_ENV] || suffix.test}`,
   username: process.env.MYSQL_USER || 'root',
   password: process.env.MYSQL_PASSWORD || 'password',
   dialect: 'mysql',
@@ -23,6 +21,7 @@ const options = {
   },
   logging: false,
 };
+
 
 module.exports = {
   development: {
